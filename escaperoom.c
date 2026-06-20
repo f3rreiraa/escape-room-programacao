55803c211fead45c4cef9e3ab086cf263ad74012
Victor <vhf.matos123@gmail.com>
2026-06-20 15:44:12 -0300

    Adiciona pistas das runas para o jogador

--- escaperoom.c
+++ escaperoom.c
@@ -4,14 +4,25 @@
 
 int main() {
     printf("=== A Cripta do Rei Esquecido ===\n");
-    return 0;
-
-printf("\nVoce se depara com uma enorme porta de pedra.\n");
+    printf("\nVoce se depara com uma enorme porta de pedra.\n");
     printf("Simbolos antigos cobrem as paredes ao redor dela.\n");
     printf("Uma voz ecoa: 'Decifre o codigo das runas e a cripta se abrira...'\n\n");
 
-srand(time(0));
-    int codigo = rand() % 41 + 10; 
+    srand(time(0));
+    int codigo = rand() % 41 + 10;
 
+printf("=== AS RUNAS REVELAM ===\n");
+    if (codigo % 2 == 0)
+        printf("- O codigo e um numero PAR\n");
+    else
+        printf("- O codigo e um numero IMPAR\n");
 
+    if (codigo <= 30)
+        printf("- O codigo nao passa de 30\n");
+    else
+        printf("- O codigo e maior que 30\n");
+
+    printf("- O codigo esta entre 10 e 50\n\n");
+
+    return 0;
 }
