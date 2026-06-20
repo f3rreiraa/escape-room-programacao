e13116bc16fb3b0d013798cbda94361132b91c50
Victor <vhf.matos123@gmail.com>
2026-06-20 15:48:31 -0300

    Adiciona opcao de jogar novamente

--- escaperoom.c
+++ escaperoom.c
@@ -3,50 +3,60 @@
 #include <time.h>
 
 int main() {
-    printf("=== A Cripta do Rei Esquecido ===\n");
-    printf("\nVoce se depara com uma enorme porta de pedra.\n");
-    printf("Simbolos antigos cobrem as paredes ao redor dela.\n");
-    printf("Uma voz ecoa: 'Decifre o codigo das runas e a cripta se abrira...'\n\n");
+    char resposta;
 
-    srand(time(0));
-    int codigo = rand() % 41 + 10;
+    do {
+        printf("=== A Cripta do Rei Esquecido ===\n");
+        printf("\nVoce se depara com uma enorme porta de pedra.\n");
+        printf("Simbolos antigos cobrem as paredes ao redor dela.\n");
+        printf("Uma voz ecoa: 'Decifre o codigo das runas e a cripta se abrira...'\n\n");
 
-printf("=== AS RUNAS REVELAM ===\n");
-    if (codigo % 2 == 0)
-        printf("- O codigo e um numero PAR\n");
-    else
-        printf("- O codigo e um numero IMPAR\n");
+        srand(time(0));
+        int codigo = rand() % 41 + 10;
 
-    if (codigo <= 30)
-        printf("- O codigo nao passa de 30\n");
-    else
-        printf("- O codigo e maior que 30\n");
+        printf("=== AS RUNAS REVELAM ===\n");
+        if (codigo % 2 == 0)
+            printf("- O codigo e um numero PAR\n");
+        else
+            printf("- O codigo e um numero IMPAR\n");
 
-    printf("- O codigo esta entre 10 e 50\n\n");
+        if (codigo <= 30)
+            printf("- O codigo nao passa de 30\n");
+        else
+            printf("- O codigo e maior que 30\n");
 
+        printf("- O codigo esta entre 10 e 50\n\n");
 
-int tentativas = 3;
-    int tentativa;
+        int tentativas = 3;
+        int tentativa;
 
-    while (tentativas > 0) {
-        printf("Tentativas restantes: %d\n", tentativas);
-        printf("Insira o codigo das runas: ");
-        scanf("%d", &tentativa);
+        while (tentativas > 0) {
+            printf("Tentativas restantes: %d\n", tentativas);
+            printf("Insira o codigo das runas: ");
+            scanf("%d", &tentativa);
 
-        if (tentativa == codigo) {
-            printf("\nAs runas brilham! A porta de pedra se abre lentamente...\n");
-            printf("Voce adentra a Cripta do Rei Esquecido.\n");
-            return 0;
-        } else if (tentativa < codigo) {
-            printf("As runas piscam. O codigo e maior.\n\n");
-        } else {
-            printf("As runas piscam. O codigo e menor.\n\n");
+            if (tentativa == codigo) {
+                printf("\nAs runas brilham! A porta de pedra se abre lentamente...\n");
+                printf("Voce adentra a Cripta do Rei Esquecido.\n");
+                break;
+            } else if (tentativa < codigo) {
+                printf("As runas piscam. O codigo e maior.\n\n");
+            } else {
+                printf("As runas piscam. O codigo e menor.\n\n");
+            }
+            tentativas--;
         }
-        tentativas--;
-    }
 
-    printf("\nAs runas se apagam. A magia te expulsa da cripta.\n");
-    printf("O codigo era %d. A cripta permanece selada.\n", codigo);
+        if (tentativas == 0) {
+            printf("\nAs runas se apagam. A magia te expulsa da cripta.\n");
+            printf("O codigo era %d. A cripta permanece selada.\n", codigo);
+        }
 
+        printf("\nDeseja tentar decifrar outra cripta? (s/n): ");
+        scanf(" %c", &resposta);
+
+    } while (resposta == 's' || resposta == 'S');
+
+    printf("\nVoce parte em busca de novas aventuras...\n");
     return 0;
 }
